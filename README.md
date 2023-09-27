c80ba2ba808cc60701a04ed3b7a9d426c325f532
caio <caionthh@hotmail.com>
2023-09-27 10:55:25 -0400

    Resolução

---
+++ README.md
@@ -0,0 +1,6 @@
+### Resolução do desafio Docker Node + Nginx + Mysql
+
+## Utilização
+use "docker-compose up -d" para subir a aplicação.
+acesse "localhost:8080" para acessar a página.
+opcionalmente é possivel alterar o nome adicionado no banco de dados adicionando "/nome" ao final do endereço, trocando "nome" pelo nome que deseja adicionar
--- node/server.js
+++ node/server.js
@@ -1,10 +1,58 @@
 const express = require('express')
+const mysql = require('mysql');
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+const config={
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+connection.query('CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id))',
+    (err, result, fields) => {
+        if (err) throw err;
+        console.log("Tabela people criada");
+    }
+);
+
+const fetchSql = `SELECT name FROM people`;
+//connection.end()
+
+const insertQuery = (insertName) => {
+    const insertSql = `INSERT INTO people(name) VALUES('${insertName}')`;
+    connection.query(insertSql);
+}
+
+
+app.get(['/', '/:n'], (req, res) => {
+    let insertName = req.params.n;
+    if (insertName === undefined)
+        insertName = 'Caio'
+
+    insertQuery(insertName);
+
+    const names = [];
+
+    connection.query(fetchSql, (err, result, fields) => {
+        if (err) throw err;
+
+        result.map(row => {
+            names.push(row.name);
+        })
+
+        const html = `
+            <h1>Full Cycle Rocks!</h1>
+            <ul>${names.map(name => `<li>${name}</li>`).join('')}
+            </ul>
+        `;
+
+        res.send(html);
+    });
+
 })
 
 app.listen(port, () => {
